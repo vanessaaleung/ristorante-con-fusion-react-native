6ceb9107d278761f90fe04b3798dbe46327d0790
Eric <leungkachiiiii@gmail.com>
2020-07-24 00:52:54 -0700

    week1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Icon } from 'react-native-elements';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 // create navigation between components
 const MenuNavigator = createStackNavigator();
 const HomeNavigator = createStackNavigator();
+const ContactNavigator = createStackNavigator();
+const AboutNavigator = createStackNavigator();
 const MainNavigator = createDrawerNavigator();
 
 function MenuNavigatorScreen() {
@@ -47,6 +50,40 @@ function HomeNavigatorScreen() {
   );
 }
 
+function ContactNavigatorScreen() {
+  return (
+    <ContactNavigator.Navigator 
+                            screenOptions={{
+                              headerStyle: {
+                                  backgroundColor: "black"
+                              },
+                              headerTintColor: "#fff",
+                              headerTitleStyle: {
+                                  color: "#fff"            
+                              }
+                            }}>
+      <ContactNavigator.Screen name="Contact" component={Contact} />
+    </ContactNavigator.Navigator>
+  );
+}
+
+function AboutNavigatorScreen() {
+  return (
+    <AboutNavigator.Navigator 
+                            screenOptions={{
+                              headerStyle: {
+                                  backgroundColor: "black"
+                              },
+                              headerTintColor: "#fff",
+                              headerTitleStyle: {
+                                  color: "#fff"            
+                              }
+                            }}>
+      <AboutNavigator.Screen name="About" component={About} />
+    </AboutNavigator.Navigator>
+  );
+}
+
 function MainNavigatorScreen() {
   return (
     <MainNavigator.Navigator openByDefault
@@ -59,10 +96,20 @@ function MainNavigatorScreen() {
                               title: 'Home',
                               drawerLabel: 'Home'
                             }} />
+      <MainNavigator.Screen name="About" component={AboutNavigatorScreen}
+                            options={{ 
+                              title: 'About Us',
+                              drawerLabel: 'About Us'
+                            }} />
       <MainNavigator.Screen name="Menu" component={MenuNavigatorScreen}
                             options={{ 
                               title: 'Menu',
                               drawerLabel: 'Menu'
+                            }} />
+      <MainNavigator.Screen name="Contact" component={ContactNavigatorScreen}
+                            options={{ 
+                              title: 'Contact Us',
+                              drawerLabel: 'Contact Us'
                             }} />
     </MainNavigator.Navigator>
   );
