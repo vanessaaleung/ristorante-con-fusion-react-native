d810d13cc956b1bcb79d48b81588690587c0a85d
Eric <leungkachiiiii@gmail.com>
2020-07-27 15:52:47 -0700

    adding favorites

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -3,13 +3,19 @@ import { View, Text, ScrollView, FlatList } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from "../shared/baseUrl";
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
     dishes: state.dishes,
-    comments: state.comments
+    comments: state.comments,
+    favorites: state.favorites
   }
-}
+};
+
+const mapDispatchToProps = dispatch => ({
+  postFavorite: (dishId) => dispatch(postFavorite(dishId))
+});
 
 function RenderDish(props) {
   const dish = props.dish;
@@ -62,18 +68,8 @@ function RenderComments(props) {
 
 class Dishdetail extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      favorites: []
-    }
-  }
-
   markFavorite(dishId) {
-    this.setState({ 
-      favorites: this.state.favorites.concat(dishId) 
-    })
+    this.props.postFavorite(dishId);
   }
 
   render() {
@@ -81,16 +77,15 @@ class Dishdetail extends Component {
     return (
       <ScrollView>
         <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                    favorite={this.state.favorites.some(
-                                ele => ele === dishId)} 
+                    favorite={this.props.favorites.some(
+                                element => element === dishId)} 
                     onPress={() => this.markFavorite(dishId)}
                     />
-        <RenderComments comments={this.props.comments.comments.filter(
-          (comment) => comment.dishId === dishId)} />
+        <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
       </ScrollView>
     );
   }
   
 }
 
-export default connect(mapStateToProps)(Dishdetail);
+export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -146,3 +146,17 @@ export const addLeaders = (leaders) => ({
   type: ActionTypes.ADD_LEADERS,
   payload: leaders
 });
+
+// favorites
+export const postFavorite = (dishId) => (dispatch) => {
+  // simulate server delay
+  setTimeout(() => {
+    dispatch(addFavorite(dishId));
+  }, 2000);
+};
+
+export const addFavorite = (dishId) => ({
+  type: ActionTypes.ADD_FAVORITE,
+  payload: dishId
+});
+
